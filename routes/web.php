09b1ba303f78dce69ee189719026d0c4dc1b4999
Ivan Rodriguez <ivanrenescorcia@gmail.com>
2020-12-19 08:28:31 -0500

    Clear Cache fun added

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/clear-cache', function() {
+    Artisan::call('cache:clear');
+    return "Cache is cleared";
+});
