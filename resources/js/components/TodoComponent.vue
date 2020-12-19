79c736999b9f38e9594f14c21d1eca9e810d3776
Ivan Rodriguez <ivanrenescorcia@gmail.com>
2020-12-19 08:27:34 -0500

    get all, save method added

--- resources/js/components/TodoComponent.vue
+++ resources/js/components/TodoComponent.vue
@@ -1,13 +1,18 @@
 <template>
-    <div class="container">
-        <form>
+    <div class="w-50">
+        <form @submit.prevent="saveData">
             <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
+            <input v-model="form.title" type="text" class="form-control  form-control-lg"  aria-label="Recipient's username" aria-describedby="basic-addon2">
             <div class="input-group-append">
-                <button class="btn btn-outline-secondary" type="button">Button</button>
+                <button class="btn btn-success" type="submit">Add this to your list</button>
             </div>
             </div>
         </form>
+        <div  class="w-25">
+            <div v-for="todo in todos" :key="todo.id" clas="w-100">
+                {{todo.title}}
+            </div>
+        </div>
     </div>
 </template>
 
@@ -15,12 +20,34 @@
     export default {
         data(){
             return{
+                todos:'',
+                form: new Form({
+                    title: '',
+                })
 
+            }
+        },
+        methods:{
+            getTodos(){
+                axios.get('/api/todo').then((res) =>{
+                    this.todos = res.data;
+                }).catch((error) => {
+                    console.log(error)
+                })
+            },
+            saveData(){
+              let data = new FormData();
+              data.append('title', this.form.title);
+              axios.post('/api/todo', data).then((res) =>{
+                      this.getTodos();
+              }).catch((error) => {
+                    console.log(error)
+                });
             }
         },
 
         mounted() {
-
+            this.getTodos();
         }
     }
 </script>
