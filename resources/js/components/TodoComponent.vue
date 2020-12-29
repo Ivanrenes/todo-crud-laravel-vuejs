79d62c11d33e6f882586e73f2cd9597a113a2ca3
ivancode <ivanrenescorcia@gmail.com>
2020-12-29 14:22:10 -0500

    nothing changed

--- resources/js/components/TodoComponent.vue
+++ resources/js/components/TodoComponent.vue
@@ -14,7 +14,7 @@
             <div v-for="todo in todos" :key="todo.id" class="w-100 d-flex align-items-center p-3 bg-white border-bottom">
                 <span class="mr-2">
 
-            <svg v-if="todo.completed == false" v-on:click="toggleTodo(todo)" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-check" width="60" height="60" viewBox="0 0 24 24" stroke-width="1.5" stroke="#7bc62d" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <svg v-if="todo.completed === false" v-on:click="toggleTodo(todo)" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-check" width="60" height="60" viewBox="0 0 24 24" stroke-width="1.5" stroke="#7bc62d" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
             <path d="M5 12l5 5l10 -10" />
             </svg>
